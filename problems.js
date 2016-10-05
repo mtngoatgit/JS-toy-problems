a0a51d249a43f56536f05bb1eb7040d726800fe7
Paxton Bigler <paxton.bigler@gmail.com>
2016-10-05 11:49:25 -0600

    Update problems.js

--- problems.js
+++ problems.js
@@ -58,6 +58,26 @@ lastName = name.shift().split("").shift()
   return firstName + '.' + lastName
 }
 
+***********************I love you, a little , a lot, passionately ... not at all**************************
+
+// Who remembers the time where in the schoolyard, girls take flower petals. Tears saying:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal torn comes to the word 'madness', these are cries of excitement, dreams and emotions surging in thoughts.
+
+// Your goal in this kata is to define the result of such a game given nb_petals > 0, the number of petals torn.
+  
+const howMuchILoveYou=n=>['not at all','I love you','a little','a lot','passionately','madly'][n%6]
+// it operates differently in repl.it. There you have to do n%6 -1
+// howMuchILoveYou is the name of the function
+// n is the argument/parameter
+// then you have an anonymous array, from which you return n%6
+// n%6 takes the argument, does modulus and returns the remainder, which is the proper index of the array
 
 
 
