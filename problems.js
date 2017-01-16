9587d4f852558a48e3d6c34a81e22afdbe243763
Paxton Bigler <paxton.bigler@gmail.com>
2017-01-16 07:40:26 -0700

    Update problems.js

--- problems.js
+++ problems.js
@@ -175,3 +175,17 @@ function reverse(string){
   return string.split(' ').reverse().join(' ')
 }
 
+************************Basic Mathmatical Operations**********************************
+// The function should take three arguments - operation(string/char), value1(number), value2(number). 
+// The function should return result of numbers after applying the chosen operation.
+  
+function basicOp(operation, value1, value2) {
+  console.log(operation, value1, value2);
+  if (operation == '+') return value1 + value2;
+  if (operation == '*') return value1 * value2;
+  if (operation == '/') return value1 / value2;
+  if (operation == '-') return value1 - value2;
+}
+
+
+
