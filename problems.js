0bed08ce6a350f10cf38076eccbdc70ac6cadb86
Paxton Bigler <paxton.bigler@gmail.com>
2017-01-16 08:03:03 -0700

    Update problems.js

--- problems.js
+++ problems.js
@@ -204,3 +204,9 @@ function fakeBin(x){
 //ES6 solution:
 fakeBin = x = x.split('').map(n => n < 5 ? 0 : 1).join('');
 
+*******************************Convert a string to an array****************************
+// Write a function to split a string and convert it into an array of words.
+  
+stringToArray = x => x.split(' ');
+
+
